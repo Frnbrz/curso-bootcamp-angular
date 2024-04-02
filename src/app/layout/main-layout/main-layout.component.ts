f343467201b5ed5e918c94d5293b9465aea6ffc7
FrnBrz <frnbrz@gmail.com>
2024-04-02 17:37:29 +0200

    Add main layout component

---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { ToolBarComponent } from '../../components/tool-bar/tool-bar.component'
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, ToolBarComponent],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss'
+})
+export class MainLayoutComponent {
+
+}
