6a5f4fd26d1e90872425f7316b364ba0209882ab
FrnBrz <frnbrz@gmail.com>
2024-04-02 17:37:09 +0200

    Add login button to toolbar

--- src/app/components/tool-bar/tool-bar.component.ts
+++ src/app/components/tool-bar/tool-bar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { LoginButtonComponent } from '../login-button/login-button.component'
 
 @Component({
   selector: 'app-tool-bar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, LoginButtonComponent],
   templateUrl: './tool-bar.component.html',
   styleUrl: './tool-bar.component.scss'
 })
