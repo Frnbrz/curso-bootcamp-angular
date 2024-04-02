4c28806369e7c49f0797afd2688f3b1bf263f1bd
FrnBrz <frnbrz@gmail.com>
2024-04-02 13:14:57 +0200

    Add contact-detail-page and contacts-page components

---
+++ src/app/pages/contact-detail-page/contact-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-detail-page.component.html',
+  styleUrl: './contact-detail-page.component.scss'
+})
+export class ContactDetailPageComponent {
+
+}
