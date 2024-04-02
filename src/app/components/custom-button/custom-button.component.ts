7027574dfd736a6859a29f1f5daf88b232950793
FrnBrz <frnbrz@gmail.com>
2024-04-02 17:36:56 +0200

    Add custom button component with HTML, SCSS, spec, and TypeScript files

---
+++ src/app/components/custom-button/custom-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+
+@Component({
+  selector: 'app-custom-button',
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.scss'
+})
+export class CustomButtonComponent {
+  @Input() url: string = ''
+}
