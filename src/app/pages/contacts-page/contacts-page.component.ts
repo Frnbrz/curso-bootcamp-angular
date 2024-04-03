9ed59e2d91dd1a1d4a074d24e5fc11259ebb969e
FrnBrz <frnbrz@gmail.com>
2024-04-03 11:08:23 +0200

    Refactor contacts-page component to use async pipe

--- src/app/pages/contacts-page/contacts-page.component.ts
+++ src/app/pages/contacts-page/contacts-page.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
+import { AsyncPipe, JsonPipe } from '@angular/common'
+import { Component, inject } from '@angular/core'
+import { Observable } from 'rxjs'
+import { UsersServiceService } from '../../services/users-service.service'
 
 @Component({
   selector: 'app-contacts-page',
   standalone: true,
-  imports: [MatListModule, MatDividerModule],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './contacts-page.component.html',
   styleUrl: './contacts-page.component.scss',
 })
-export class ContactsPageComponent {}
+export class ContactsPageComponent {
+  usersService = inject(UsersServiceService);
+
+  data$: Observable<any> = this.usersService.getUsers();
+}
