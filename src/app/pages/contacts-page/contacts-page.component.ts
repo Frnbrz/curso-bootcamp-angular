45bbb426f8051ff0833f740cf7246ad6edfebc75
FrnBrz <frnbrz@gmail.com>
2024-04-03 16:33:09 +0200

    Refactor contacts-page component to display user details and navigate to user details page

--- src/app/pages/contacts-page/contacts-page.component.ts
+++ src/app/pages/contacts-page/contacts-page.component.ts
@@ -1,17 +1,34 @@
-import { AsyncPipe, JsonPipe } from '@angular/common'
-import { Component, inject } from '@angular/core'
-import { Observable } from 'rxjs'
+import { Component, inject, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { CustomButtonComponent } from '../../components/custom-button/custom-button.component'
 import { UsersServiceService } from '../../services/users-service.service'
 
 @Component({
   selector: 'app-contacts-page',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
+  imports: [CustomButtonComponent],
   templateUrl: './contacts-page.component.html',
   styleUrl: './contacts-page.component.scss',
 })
-export class ContactsPageComponent {
-  usersService = inject(UsersServiceService);
+export class ContactsPageComponent implements OnInit {
+  private usersService = inject(UsersServiceService);
+  router = inject(Router);
+  users: any[] = []
 
-  data$: Observable<any> = this.usersService.getUsers();
+  ngOnInit(): void {
+    this.loadUsers()
+  }
+
+  loadUsers() {
+    this.usersService.getUsers()
+      .subscribe((users: any) => {
+        this.users = users
+      })
+  }
+
+
+
+  moreUserDetails(id: string) {
+    this.router.navigate(['/contact', id])
+  }
 }
