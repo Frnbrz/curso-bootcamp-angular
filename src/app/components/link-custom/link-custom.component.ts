27a00ea8090c46e8ca406979811401c95c72d1c2
FrnBrz <frnbrz@gmail.com>
2024-04-03 08:42:29 +0200

    Update link-custom component to use routerLink instead of href

--- src/app/components/link-custom/link-custom.component.ts
+++ src/app/components/link-custom/link-custom.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, Input } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-link-custom',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, RouterModule],
   templateUrl: './link-custom.component.html',
   styleUrl: './link-custom.component.scss',
 })
