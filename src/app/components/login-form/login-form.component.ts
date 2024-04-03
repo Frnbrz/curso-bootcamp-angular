773b6263ebf4d8f01cfc6884b75a0d748297876e
FrnBrz <frnbrz@gmail.com>
2024-04-03 09:57:19 +0200

    Update login form component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { AsyncPipe, JsonPipe } from '@angular/common'
+import { Component, inject } from '@angular/core'
 import {
   FormControl,
   FormGroupDirective,
@@ -10,6 +11,8 @@ import {
 import { ErrorStateMatcher } from '@angular/material/core'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { LoginRequestService } from '../../services/login-request.service'
+import { CustomButtonComponent } from '../custom-button/custom-button.component'
 
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -22,12 +25,21 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, CustomButtonComponent, JsonPipe, AsyncPipe],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  LoginService = inject(LoginRequestService)
+
+
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   passwordFormControl = new FormControl('', [Validators.required]);
   matcher = new MyErrorStateMatcher();
+
+  constructor() { }
+
+  login() {
+    console.log('login')
+  }
 }
