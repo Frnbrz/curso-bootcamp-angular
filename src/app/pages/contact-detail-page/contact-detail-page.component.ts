2cfc3618069013af494986b28d5719128a953a4d
FrnBrz <frnbrz@gmail.com>
2024-04-03 16:33:04 +0200

    Add user details to contact detail page

--- src/app/pages/contact-detail-page/contact-detail-page.component.ts
+++ src/app/pages/contact-detail-page/contact-detail-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { UserServiceService } from '../../services/user-service.service'
 
 @Component({
   selector: 'app-contact-detail-page',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './contact-detail-page.component.html',
   styleUrl: './contact-detail-page.component.scss',
 })
-export class ContactDetailPageComponent {}
+export class ContactDetailPageComponent implements OnInit {
+
+  private activatedRoute = inject(ActivatedRoute);
+  private userServuce = inject(UserServiceService);
+  user: any
+
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.loadUser(params['id'])
+    })
+  }
+
+  loadUser(id: number) {
+    this.userServuce.getUsers(id)
+      .subscribe((user: any) => {
+        this.user = user
+      })
+
+
+  }
+
+}
+
+
+
+
