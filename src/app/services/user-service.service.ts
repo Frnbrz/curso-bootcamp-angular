ade70153f0a738392d632e9581163343561c9a46
FrnBrz <frnbrz@gmail.com>
2024-04-03 16:33:14 +0200

    Add user service unit test and implement user service

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,17 +1,16 @@
 import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
 
-
-const URL = 'https://jsonplaceholder.typicode.com/users'
-
+const URL = 'https://jsonplaceholder.typicode.com/users/'
 @Injectable({
   providedIn: 'root'
 })
-export class UsersServiceService {
+export class UserServiceService {
+
   private http = inject(HttpClient);
   constructor() { }
 
-  getUsers() {
-    return this.http.get(URL)
+  getUsers(id: number) {
+    return this.http.get(`${URL}${id}`)
   }
 }
