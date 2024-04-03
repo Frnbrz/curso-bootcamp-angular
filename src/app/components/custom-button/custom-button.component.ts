7a127d5776012345ff797c2009e1a759312cc1db
FrnBrz <frnbrz@gmail.com>
2024-04-03 09:57:11 +0200

    Refactor custom button component

--- src/app/components/custom-button/custom-button.component.ts
+++ src/app/components/custom-button/custom-button.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, Input } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-custom-button',
