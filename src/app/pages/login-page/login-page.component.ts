8fb0fcc533f02fa02609039787ccd23ec69ab464
FrnBrz <frnbrz@gmail.com>
2024-04-04 15:19:00 +0200

    Add login functionality and redirect to contacts page if token exists

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 import { LoginFormComponent } from '../../components/login-form/login-form.component'
 
 @Component({
@@ -8,4 +9,17 @@ import { LoginFormComponent } from '../../components/login-form/login-form.compo
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent implements OnInit {
+  token: string | null = window.localStorage.getItem('token') || null;
+  router = inject(Router);
+  constructor() { }
+
+  ngOnInit(): void {
+    if (
+      this.token !== null
+    ) {
+      this.router.navigate(['/contacts'])
+    }
+
+  }
+}
--- src/app/services/login-request.service.ts
+++ src/app/services/login-request.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable, inject } from '@angular/core'
+import { Observable } from 'rxjs'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginRequestService {
   http = inject(HttpClient);
-  loginRequest() {
+  loginRequest(email: string, password: string): Observable<any> {
     return this.http.post('https://nest-frnbrz-api.onrender.com/api/v1/auth/login', {
-      email: 'a@a.com',
-      password: '123'
+      email,
+      password
     },
       {
         headers: {
