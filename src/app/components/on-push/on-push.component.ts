6bb14f18db6b775c4fd6aa555c7f38ff07c66776
FrnBrz <frnbrz@gmail.com>
2024-04-13 11:17:02 +0200

    Update on-push component to use ChangeDetectionStrategy.OnPush

--- src/app/components/on-push/on-push.component.ts
+++ src/app/components/on-push/on-push.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-on-push',
   standalone: true,
   imports: [],
   templateUrl: './on-push.component.html',
-  styleUrl: './on-push.component.scss'
+  styleUrl: './on-push.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OnPushComponent implements OnInit {
   segundos: number = 0;
