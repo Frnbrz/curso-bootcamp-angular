a39687b52860cf93c11fe080879f68a27f10a247
FrnBrz <frnbrz@gmail.com>
2024-04-13 11:17:07 +0200

    Add VistaAvanzadaComponent and DetachComponent to VistaAvanzadaComponent imports

--- src/app/pages/vista-avanzada/vista-avanzada.component.ts
+++ src/app/pages/vista-avanzada/vista-avanzada.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { DetachComponent } from '../../components/detach/detach.component'
 import { OnPushComponent } from '../../components/on-push/on-push.component'
 
 @Component({
   selector: 'app-vista-avanzada',
   standalone: true,
-  imports: [OnPushComponent],
+  imports: [OnPushComponent, DetachComponent],
   templateUrl: './vista-avanzada.component.html',
   styleUrl: './vista-avanzada.component.scss'
 })
